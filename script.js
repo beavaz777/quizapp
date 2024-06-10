e59472fa0cc12b9ec2941b0448a263939a701102
Beatriz Vaz <beavazxp@gmail.com>
2024-06-10 19:43:45 +0000

    voltando de onde tudo deu errado

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+let tema = "claro"
+
 const botaoTema = document.querySelector(".tema button")
+botaoTema.addEventListener("click", trocarTema)
+
 const body = document.querySelector("body")
-botaoTema.addEventListener("click", () => {
-    trocarTema(body, botaoTema)
-})
 
-verificarTema(body, botaoTema)
-verificarTema(body, botaoTema)
-
-const botoesAssunto = document.querySelectorAll(".assuntos button")
-botoesAssunto.forEach(botao => {
-    botao.addEventListener("click", selecionarAssunto)
-})
-
-function selecionarAssunto(evento) {
-    const assunto = evento.target.innerText
-    localStorage.setItem("assunto", assunto)
-    window.location.href = "./pages/quiz/quiz.html"
+function trocarTema() {
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+    
+    if (tema === "claro") {
+        body.classList.add("escuro")
+        localStorage.setItem("tema", "escuro")
+    } else {
+        body.classList.remove("escuro")
+        localStorage.setItem("tema", "claro")
+    }
 }
+
+function verificarTema() {
+    if (localStorage.getItem("tema")) {
+        tema = localStorage.getItem("tema")
+    }
+
+    if (tema === "escuro") {
+        body.classList.add("escuro")
+    }
+}
+
+verificarTema()
